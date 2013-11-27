6b9cc53812c6c9eea5a585057c9b3b0c30235802
Laszlo Pandy <laszlo.pandy@prezi.com>
2013-11-27 11:29:59 +0100

    Fix TypeScript function type chain.

--- spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/AbstractTypeScriptGeneratorVisitor.groovy
+++ spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/AbstractTypeScriptGeneratorVisitor.groovy
@@ -70,12 +70,11 @@ abstract class AbstractTypeScriptGeneratorVisitor extends AbstractModuleVisitor<
 	}
 
 	@Override
-	String visitNormalTypeChain(@NotNull @NotNull ModuleParser.CallbackTypeChainContext ctx)
+	String visitCallbackTypeChain(@NotNull @NotNull ModuleParser.CallbackTypeChainContext ctx)
 	{
         def typeChain = ctx.returnType()
         def lastType = typeChain.remove(typeChain.size() - 1)
 
-        def functionDef
         if (typeChain.size() == 0) {
             return lastType.accept(this);
         }
