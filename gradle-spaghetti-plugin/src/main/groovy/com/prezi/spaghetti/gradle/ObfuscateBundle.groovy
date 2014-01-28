c068d71bc733acf8840f7edec7cf5b2debd0d430
Ex Falso <0slemi0@gmail.com>
2014-01-28 18:10:59 +0000

    Fixed ModuleBundle call

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ObfuscateBundle.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ObfuscateBundle.groovy
@@ -9,118 +9,117 @@ import org.gradle.api.tasks.TaskAction
 
 class ObfuscateBundle extends AbstractBundleTask
 {
-  private static final List<String> s_protectedSymbols = ["define", "prototype", "__consts"];
+	private static final List<String> s_protectedSymbols = ["define", "prototype", "__consts"];
 
-  private enum With
-  {
-    Closure, UglifyJS
-  };
+	private enum With
+	{
+		Closure, UglifyJS
+	};
 
-  private With d_with;
+	private With d_with;
 
-  public void withClosure()
-  {
-    d_with = With.Closure;
-  }
+	public void withClosure()
+	{
+		d_with = With.Closure;
+	}
 
-  public void withUglifyJS()
-  {
-    d_with = With.UglifyJS;
-  }
+	public void withUglifyJS()
+	{
+		d_with = With.UglifyJS;
+	}
 
-  private List<String> uglifyJSCommandLine(Set<String> symbols, File uglifyFile, File mapJStoMinFile)
-  {
+	private List<String> uglifyJSCommandLine(Set<String> symbols, File uglifyFile, File mapJStoMinFile)
+	{
 
-    return ["uglifyjs", uglifyFile, "--compress", "--mangle",
-            "--reserved=" + symbols.join(","),
-            "--source-map=" + mapJStoMinFile,
-            "--source-map-url=''"];
-  }
+		return ["uglifyjs", uglifyFile, "--compress", "--mangle",
+				"--reserved=" + symbols.join(","),
+				"--source-map=" + mapJStoMinFile,
+				"--source-map-url=''"];
+	}
 
-  private List<String> closureCommandLine(File closureFile, File mapJStoMinFile)
-  {
+	private List<String> closureCommandLine(File closureFile, File mapJStoMinFile)
+	{
 
-    return ["closure", "--js=" + closureFile, "--compilation_level=ADVANCED_OPTIMIZATIONS",
-            "--create_source_map=" + mapJStoMinFile];
-  }
+		return ["closure", "--js=" + closureFile, "--compilation_level=ADVANCED_OPTIMIZATIONS",
+				"--create_source_map=" + mapJStoMinFile];
+	}
 
-  public ObfuscateBundle()
-  {
-    d_with = With.UglifyJS;
-    inputFile = new File(project.buildDir, "spaghetti/module.zip");
-    outputFile = new File(project.buildDir, "spaghetti/module_obf.zip");
-  }
+	public ObfuscateBundle()
+	{
+		d_with = With.UglifyJS;
+		inputFile = new File(project.buildDir, "spaghetti/module.zip");
+		outputFile = new File(project.buildDir, "spaghetti/module_obf.zip");
+	}
 
-  @TaskAction
-  void run()
-  {
+	@TaskAction
+	void run()
+	{
 
-    def config = readConfig(definition.text);
-    def modules = config.localModules + config.getDependentModules();
-    def cmdLine = "";
-    def obfuscateDir = new File(project.buildDir, "obfuscate");
-    obfuscateDir.mkdirs();
-    def mapJStoMinFile = new File(obfuscateDir, "JStoMin.map");
-    Set<String> symbols = s_protectedSymbols + modules.collect{new SymbolCollectVisitor().visit(it.context)}.flatten();
+		def config = readConfig(definition.text);
+		def modules = config.localModules + config.getDependentModules();
+		def cmdLine = "";
+		def obfuscateDir = new File(project.buildDir, "obfuscate");
+		obfuscateDir.mkdirs();
+		def mapJStoMinFile = new File(obfuscateDir, "JStoMin.map");
+		Set<String> symbols = s_protectedSymbols + modules.collect{new SymbolCollectVisitor().visit(it.context)}.flatten();
 
-    def bundle = ModuleBundle.load(inputFile);
+		def bundle = ModuleBundle.load(inputFile);
 
-    // OBFUSCATE
-    switch (d_with)
-    {
-    case With.Closure: _: {
-      def closureFile = new File(obfuscateDir, "closure.js");
-      closureFile.delete();
-      closureFile << bundle.bundledJavaScript << "\nvar __a = {};\n" + symbols.collect{
-        "/** @expose */\n__a." + it + " = {};\n"
-      }.join("");
+		// OBFUSCATE
+		switch (d_with)
+		{
+		case With.Closure: _: {
+			def closureFile = new File(obfuscateDir, "closure.js");
+			closureFile.delete();
+			closureFile << bundle.bundledJavaScript << "\nvar __a = {};\n" + symbols.collect{
+				"/** @expose */\n__a." + it + " = {};\n"
+			}.join("");
 
-      cmdLine = closureCommandLine(closureFile, mapJStoMinFile);
-      break;
-    }
-    case With.UglifyJS: _: {
-      def uglifyFile = new File(obfuscateDir, "uglify.js");
-      uglifyFile.delete();
-      uglifyFile << bundle.bundledJavaScript;
-      cmdLine = uglifyJSCommandLine(symbols, uglifyFile, mapJStoMinFile);
-      break;
-    }
-    default: throw new RuntimeException("Invalid d_with: " + d_with);
-    };
+			cmdLine = closureCommandLine(closureFile, mapJStoMinFile);
+			break;
+		}
+		case With.UglifyJS: _: {
+			def uglifyFile = new File(obfuscateDir, "uglify.js");
+			uglifyFile.delete();
+			uglifyFile << bundle.bundledJavaScript;
+			cmdLine = uglifyJSCommandLine(symbols, uglifyFile, mapJStoMinFile);
+			break;
+		}
+		default: throw new RuntimeException("Invalid d_with: " + d_with);
+		};
 
-    println("Protected API symbols: " + symbols.join(","))
-    println("Executing \"" + cmdLine.join(" ") + "\"...");
+		println("Protected API symbols: " + symbols.join(","))
+		println("Executing \"" + cmdLine.join(" ") + "\"...");
 
-    def process = cmdLine.execute();
-    def compressedJS = new StringBuilder();
-    process.waitForProcessOutput(compressedJS, System.err);
+		def process = cmdLine.execute();
+		def compressedJS = new StringBuilder();
+		process.waitForProcessOutput(compressedJS, System.err);
 
-    if (process.exitValue() != 0)
-  {
-    throw new RuntimeException("Obfuscation failed with exit code " + process.exitValue());
-  }
+		if (process.exitValue() != 0) {
+			throw new RuntimeException("Obfuscation failed with exit code " + process.exitValue());
+		}
 
-  // SOURCEMAP
-  def mapJStoMin = mapJStoMinFile.text;
-  def finalSourceMap;
-  if (bundle.sourceMap != null)
-  {
-    finalSourceMap = SourceMap.compose(bundle.sourceMap, mapJStoMin, "module.map");
-  }
-  else
-  {
-    finalSourceMap = mapJStoMin;
-  }
+		// SOURCEMAP
+		def mapJStoMin = mapJStoMinFile.text;
+		def finalSourceMap;
+		if (bundle.sourceMap != null)
+		{
+			finalSourceMap = SourceMap.compose(bundle.sourceMap, mapJStoMin, "module.map");
+		}
+		else
+		{
+			finalSourceMap = mapJStoMin;
+		}
 
-  // BUNDLE
-  def obfBundle = new ModuleBundle(bundle.name, bundle.definition, compressedJS.toString(), finalSourceMap);
-  obfBundle.save(outputFile);
-  }
+		// BUNDLE
+		def obfBundle = new ModuleBundle(bundle.name, bundle.definition, bundle.version, bundle.source, compressedJS.toString(), finalSourceMap);
+		obfBundle.save(outputFile);
+	}
 
-  @Override
-  @InputFile
-  File getDefinition()
-  {
-    return super.getDefinition()
-  }
+	@Override
+	@InputFile
+	File getDefinition()
+	{
+		return super.getDefinition()
+	}
 }
