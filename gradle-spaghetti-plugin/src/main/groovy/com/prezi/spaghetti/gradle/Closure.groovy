b646eeb88d5de36382efc720c4c0418c369353e4
Ex Falso <0slemi0@gmail.com>
2014-01-30 15:30:55 +0000

    Fixed JS primitives obfuscated by Closure

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/Closure.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/Closure.groovy
@@ -7,33 +7,52 @@ import com.google.javascript.jscomp.CompilerOptions;
 import com.google.javascript.jscomp.SourceFile;
 import com.google.javascript.jscomp.CompilationLevel;
 import com.google.javascript.jscomp.SourceMap;
+import com.google.javascript.jscomp.CommandLineRunner;
 
 class Closure {
 
-	private static CompilationLevel compilationLevel = CompilationLevel.ADVANCED_OPTIMIZATIONS;
-	private static int lineLengthThreshold = 1;
+	/* This forces closure to insert a newline in the obfuscated code
+	   every chance it gets.
 
+	   The reason we want this is because column number information in
+	   stack traces are inconsistent across browsers. Most of the time
+	   we only have line numbers, which means we cannot exactly tell
+	   where in the code the obfuscated statement in question
+	   originates from.
+
+	   This way we don't rely on support for column number information
+	   in browsers.
+	*/
+	private static final int lineLengthThreshold = 1;
+	private static CompilationLevel compilationLevel = CompilationLevel.ADVANCED_OPTIMIZATIONS;
+
+	// compiles 'jsFileName', appends the obfuscated code to 'obfuscated' and appends the source map to 'sourceMap' using 'sourceMapName' (the 'file' field in the sourcemap)
 	public static int compile(String jsFileName, Appendable obfuscated, String sourceMapName, Appendable sourceMap) {
 		def compiler = new Compiler(System.err);
 		def options = new CompilerOptions();
 
 		def js = SourceFile.fromFile(jsFileName, UTF_8);
 
+		// OPTIONS
 		compilationLevel.setOptionsForCompilationLevel(options);
 		options.setLineLengthThreshold(lineLengthThreshold);
-		options.setSourceMapOutputPath("dummy.map"); // This needs to be here only so that Closure generates sourcemap info
+		options.setSourceMapOutputPath("dummy.map"); // We need to set this so that Closure generates sourcemap info, it won't actually create a file.
 
-		def res = compiler.compile([], [js], options);
+		// Set default externs so that commonly used primitives are protected
+		def externs = CommandLineRunner.getDefaultExterns();
 
+		// COMPILE
+		def res = compiler.compile(externs, [js], options);
 		def retCode = Math.min(res.errors.length, 0x7f);
 		if (retCode != 0) {
 			return retCode;
 		}
 
+		// append results
 		obfuscated.append(compiler.toSource());
 		compiler.getSourceMap().appendTo(sourceMap, sourceMapName);
 
 		return 0;
-		
 	}
+
 }
