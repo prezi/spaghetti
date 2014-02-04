cb57c5e1824f7643983a4f551866c9470e98ccf0
exfalso <0slemi0@gmail.com>
2014-02-04 17:48:07 +0100

    Stack trace line parsing

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/StackTrace.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/StackTrace.groovy
@@ -1,10 +1,32 @@
 package com.prezi.spaghetti.gradle
 
+import java.util.regex.*;
+
 public class StackTrace {
+
+	private class LineMatcher {
+		public Pattern pattern;
+		public int lineIx, jsIx; // index of regex groups
+	}
+
+	private static final List<LineMatcher> lmatchers =
+		[
+			// chrome copy-paste stack,
+			// chrome console stack,
+			// ff console stack
+			[pattern : ~/([a-zA-Z0-9._-]*)\.js:(\d+)/,
+			 lineIx : 2,
+			 jsIx : 1]
+		];
 
 	public class LineInfo {
 		public int lineNo;
-		public String moduleName;
+		public String jsName;
+
+		public LineInfo(int lineNo, String jsName) {
+			this.lineNo = lineNo;
+			this.jsName = jsName;
+		}
 	}
 
 	public List<LineInfo> lines;
@@ -14,7 +36,19 @@ public class StackTrace {
 	}
 
 	public static StackTrace parse(String rawStackTrace) {
-		return new StackTrace([]);
+
+		def lines = rawStackTrace.readLines().collect{
+			for (l in lmatchers) {
+				def m = l.pattern.matcher(it);
+				if (m.size() > 0) {
+					return [lineNo : Integer.decode(m[0][l.lineIx]),
+							jsName : m[0][l.jsIx]];
+				}
+			}
+			return null;
+		};
+
+		return new StackTrace(lines);
 	}
 
 }
