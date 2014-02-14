a52a039003240477f26def3abc4f094f65572919
Lorant Pinter <lorant.pinter@prezi.com>
2014-02-15 00:15:13 +0100

    No need for async file resolution in JavaScript binary

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/SpaghettiCompatibleJavaScriptBinary.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/SpaghettiCompatibleJavaScriptBinary.groovy
@@ -8,6 +8,6 @@ import java.util.concurrent.Callable
  * Created by lptr on 11/02/14.
  */
 interface SpaghettiCompatibleJavaScriptBinary extends Binary {
-	Callable<File> getJavaScriptFile()
-	Callable<File> getSourceMapFile()
+	File getJavaScriptFile()
+	File getSourceMapFile()
 }
