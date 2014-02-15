bd6c87de8a638ccbf576df77e76530c6329543e9
Lorant Pinter <lorant.pinter@prezi.com>
2014-02-15 02:39:54 +0100

    Extract modules should not be a generate task

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ExtractModules.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ExtractModules.groovy
@@ -1,11 +1,19 @@
 package com.prezi.spaghetti.gradle
 
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 /**
  * Created by lptr on 17/11/13.
  */
-class ExtractModules extends AbstractGenerateTask {
+class ExtractModules extends AbstractSpaghettiTask {
+	@OutputDirectory
+	File outputDirectory
+
+	void outputDirectory(Object directory) {
+		this.outputDirectory = project.file(directory)
+	}
+
 	ExtractModules() {
 		this.conventionMapping.outputDirectory = { new File(project.buildDir, "spaghetti/modules") }
 	}
