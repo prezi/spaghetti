8b3527d9a0ad65bd1dccf051cb03d075fe39b48b
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-19 14:26:09 +0200

    ExtractModules is now incremental

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ExtractModules.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/ExtractModules.groovy
@@ -1,12 +1,13 @@
 package com.prezi.spaghetti.gradle
 
+import com.prezi.spaghetti.ModuleBundle
 import com.prezi.spaghetti.ModuleBundle.Elements
-import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.FileCollection
+import org.gradle.api.Action
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+import org.gradle.api.tasks.incremental.InputFileDetails
 
 /**
  * Created by lptr on 17/11/13.
@@ -36,12 +37,31 @@ class ExtractModules extends AbstractSpaghettiTask {
 	}
 
 	@TaskAction
-	extract() {
+	extract(IncrementalTaskInputs inputs) {
 		def output = getOutputDirectory()
-		output.delete() || output.deleteDir()
 		output.mkdirs()
-		logger.warn ">>>>> Bundles ${getBundles()}"
-		ModuleDefinitionLookup.getAllBundles(getBundles()).each { bundle ->
+
+		// Remove all previously created module directories that don't belong to bundles
+		def allBundleNames = getBundles()*.name
+		output.eachDir { moduleDir ->
+			if (!(moduleDir.name in allBundleNames)) {
+				logger.debug "Removing ${moduleDir} because it does not belong to a module anymore"
+				moduleDir.delete() || moduleDir.deleteDir()
+			}
+		}
+
+		// Collect which bundles were changed/added
+		def changedBundles = []
+		inputs.outOfDate(new Action<InputFileDetails>() {
+			@Override
+			void execute(InputFileDetails details) {
+				ExtractModules.this.logger.debug((details.isModified() ? "Re-extracting" : "Extracting") + " bundle file ${details.file}")
+				changedBundles.add details.file
+			}
+		})
+
+		ModuleDefinitionLookup.getAllBundles(changedBundles).each { ModuleBundle bundle ->
+			logger.info "Extracting bundle ${bundle.name}"
 			bundle.extract(new File(output, bundle.name), getElementsToExtract())
 		}
 	}
