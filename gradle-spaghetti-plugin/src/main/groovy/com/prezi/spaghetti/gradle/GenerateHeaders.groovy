6b7a13ba6153f0dbac7e29efbbbc0c8ef27f1e91
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-19 13:31:38 +0200

    Removed AbstractGenerateTask

--- gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/GenerateHeaders.groovy
+++ gradle-spaghetti-plugin/src/main/groovy/com/prezi/spaghetti/gradle/GenerateHeaders.groovy
@@ -1,10 +1,18 @@
 package com.prezi.spaghetti.gradle
 
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 /**
  * Created by lptr on 12/11/13.
  */
-class GenerateHeaders extends AbstractGenerateTask {
+class GenerateHeaders extends AbstractSpaghettiTask {
+
+	@OutputDirectory
+	File outputDirectory
+
+	void outputDirectory(Object directory) {
+		this.outputDirectory = project.file(directory)
+	}
 
 	GenerateHeaders()
 	{
