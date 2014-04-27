1bf381bb92142dda910c3bf226f8f5a0a4e2e216
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-27 21:35:51 +0200

    Fixed version loading

--- spaghetti-core/src/main/groovy/com/prezi/spaghetti/Version.groovy
+++ spaghetti-core/src/main/groovy/com/prezi/spaghetti/Version.groovy
@@ -4,5 +4,11 @@ package com.prezi.spaghetti
  * Created by lptr on 28/01/14.
  */
 final class Version {
-	public static String SPAGHETTI_VERSION = new ConfigSlurper().parse(Version.getResource("/spaghetti.properties")).version
+	public static String SPAGHETTI_VERSION = loadVersion()
+
+	private static String loadVersion() {
+		def props = new Properties()
+		props.load(Version.getResourceAsStream("/spaghetti.properties"))
+		return props.getProperty("version")
+	}
 }
