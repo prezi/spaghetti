8c548b709d7f4fe3db43444751e55246250b02e0
Lorant Pinter <lorant.pinter@prezi.com>
2014-05-16 16:30:18 +0200

    Automate protected words lookup

--- spaghetti-core/src/main/groovy/com/prezi/spaghetti/ReservedWords.groovy
+++ spaghetti-core/src/main/groovy/com/prezi/spaghetti/ReservedWords.groovy
@@ -1,14 +1,34 @@
 package com.prezi.spaghetti
 
+import java.lang.annotation.Documented
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
 /**
  * Created by lptr on 16/05/14.
  */
 class ReservedWords {
+	@Protected
 	public static final String MODULE = "__module"
+
+	@Protected
 	public static final String CONSTANTS = "__consts"
 
+	@Protected
+	public static final String SPAGHETTI_WRAPPER_FUNCTION = "__spaghetti"
+
 	// Words to be protected against obfuscation
-	public static final List<String> PROTECTED_WORDS = [
-	        MODULE, CONSTANTS
-	].asImmutable()
+	public static final SortedSet<String> PROTECTED_WORDS = new TreeSet<>(ReservedWords.declaredFields.findAll {
+		return it.type == String && it.getAnnotation(Protected)
+	}.collect { (String) it.get(ReservedWords) }).asImmutable()
 }
+
+/**
+ * Protect word against obfuscation.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+@interface Protected {}
