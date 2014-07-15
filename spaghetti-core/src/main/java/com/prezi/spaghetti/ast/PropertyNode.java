eb7a2cc3d1377c9dcf784ada6df329b35924e77f
Lorant Pinter <lorant.pinter@prezi.com>
2014-07-16 01:57:08 +0200

    PropertyNode should extend TypeNamePairNode

--- spaghetti-core/src/main/java/com/prezi/spaghetti/ast/PropertyNode.java
+++ spaghetti-core/src/main/java/com/prezi/spaghetti/ast/PropertyNode.java
@@ -1,4 +1,4 @@
 package com.prezi.spaghetti.ast;
 
-public interface PropertyNode extends AnnotatedNode, NamedNode {
+public interface PropertyNode extends AnnotatedNode, NamedNode, TypeNamePairNode<TypeReference> {
 }
