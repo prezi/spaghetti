a533aa503ac650d325147121b30fabb4a9d7e5b6
Lorant Pinter <lorant.pinter@prezi.com>
2014-07-21 23:34:05 +0200

    Store build number in JAR

--- spaghetti-core/src/main/java/com/prezi/spaghetti/Version.java
+++ spaghetti-core/src/main/java/com/prezi/spaghetti/Version.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.util.Properties;
 
 public final class Version {
-	private static String loadVersion() {
+	private static Properties props = loadProperties();
+	private static Properties loadProperties() {
 		Properties props = new Properties();
 		try {
 			props.load(Version.class.getResourceAsStream("/spaghetti.properties"));
 		} catch (IOException e) {
 			throw new RuntimeException("Unable to load spaghetti.properties");
 		}
-		return props.getProperty("version");
+		return props;
 	}
 
-	public static String SPAGHETTI_VERSION = loadVersion();
+	public static String SPAGHETTI_VERSION = props.getProperty("version");
+	public static String SPAGHETTI_BUILD = props.getProperty("build");
 }
