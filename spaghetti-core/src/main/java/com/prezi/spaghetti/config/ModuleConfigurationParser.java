2bb9a0924f170d4088a8b0112a6014801b7018f9
Lorant Pinter <lorant.pinter@prezi.com>
2014-08-10 18:24:10 +0200

    Make all defined types available while parsing modules
    Fixes #109

--- spaghetti-core/src/main/java/com/prezi/spaghetti/config/ModuleConfigurationParser.java
+++ spaghetti-core/src/main/java/com/prezi/spaghetti/config/ModuleConfigurationParser.java
@@ -2,6 +2,7 @@ package com.prezi.spaghetti.config;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import com.prezi.spaghetti.ast.ModuleNode;
 import com.prezi.spaghetti.ast.parser.AstParserException;
@@ -18,25 +19,38 @@ public class ModuleConfigurationParser {
 	public static ModuleConfiguration parse(Collection<ModuleDefinitionSource> localModuleSources, Collection<ModuleDefinitionSource> dependentModuleSources, Collection<ModuleDefinitionSource> transitiveModuleSources) {
 		Set<String> parsedModules = Sets.newLinkedHashSet();
 		DefaultModuleConfiguration configNode = new DefaultModuleConfiguration();
-		TypeResolver transitiveResolver = parseModules(MissingTypeResolver.INSTANCE, transitiveModuleSources, configNode.getTransitiveDependentModules(), parsedModules);
-		TypeResolver directResolver = parseModules(transitiveResolver, dependentModuleSources, configNode.getDirectDependentModules(), parsedModules);
-		parseModules(directResolver, localModuleSources, configNode.getLocalModules(), parsedModules);
+
+		Collection<ModuleParser> transitiveParser = createParsersFor(transitiveModuleSources);
+		Collection<ModuleParser> directParser = createParsersFor(dependentModuleSources);
+		Collection<ModuleParser> localParsers = createParsersFor(localModuleSources);
+
+		TypeResolver resolver = createResolverFor(Iterables.concat(localParsers, directParser, transitiveParser));
+
+		parsedModules(resolver, transitiveParser, configNode.getTransitiveDependentModules(), parsedModules);
+		parsedModules(resolver, directParser, configNode.getDirectDependentModules(), parsedModules);
+		parsedModules(resolver, localParsers, configNode.getLocalModules(), parsedModules);
+
 		return configNode;
 	}
 
-	public static TypeResolver parseModules(TypeResolver parentResolver, Collection<ModuleDefinitionSource> sources, final Set<ModuleNode> moduleNodes, final Set<String> allModuleNames) {
-		Collection<ModuleParser> parsers = Collections2.transform(sources, new Function<ModuleDefinitionSource, ModuleParser>() {
+	private static Collection<ModuleParser> createParsersFor(Collection<ModuleDefinitionSource> sources) {
+		return Collections2.transform(sources, new Function<ModuleDefinitionSource, ModuleParser>() {
 			@Override
 			public ModuleParser apply(ModuleDefinitionSource input) {
 				return ModuleParser.create(input);
 			}
 		});
+	}
 
-		TypeResolver resolver = parentResolver;
+	private static TypeResolver createResolverFor(Iterable<ModuleParser> parsers) {
+		TypeResolver resolver = MissingTypeResolver.INSTANCE;
 		for (ModuleParser parser : parsers) {
 			resolver = new ModuleTypeResolver(resolver, parser.getModule());
 		}
+		return resolver;
+	}
 
+	private static void parsedModules(TypeResolver resolver, Collection<ModuleParser> parsers, Collection<ModuleNode> moduleNodes, Set<String> allModuleNames) {
 		for (ModuleParser parser : parsers) {
 			ModuleNode module = parser.parse(resolver);
 			if (allModuleNames.contains(module.getName())) {
@@ -46,6 +60,5 @@ public class ModuleConfigurationParser {
 			allModuleNames.add(module.getName());
 			moduleNodes.add(module);
 		}
-		return resolver;
 	}
 }
--- spaghetti-core/src/test/groovy/com/prezi/spaghetti/config/ModuleConfigurationParserTest.groovy
+++ spaghetti-core/src/test/groovy/com/prezi/spaghetti/config/ModuleConfigurationParserTest.groovy
@@ -17,4 +17,16 @@ class ModuleConfigurationParserTest extends Specification {
 		def ex = thrown AstParserException
 		ex.message == "Parse error in C:\\test1.module: module loaded multiple times: com.example.test"
 	}
+
+	// See https://github.com/prezi/spaghetti/issues/109
+	def "Dependency accessed both directly and transitively"() {
+		when:
+		ModuleConfigurationParser.parse(
+				[new ModuleDefinitionSource("A", "module com.example.testA")],
+				[new ModuleDefinitionSource("B", "module com.example.testB struct Point { int x int y }")],
+				[new ModuleDefinitionSource("C", "module com.example.testC com.example.testB.Point origin()")]
+		)
+		then:
+		notThrown AstParserException
+	}
 }
