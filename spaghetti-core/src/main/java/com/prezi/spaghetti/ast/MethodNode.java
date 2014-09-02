cb376c4d9ba394449a247717775504b22125bff3
Lorant Pinter <lorant.pinter@prezi.com>
2014-09-02 19:42:21 +0200

    Methods are documented

--- spaghetti-core/src/main/java/com/prezi/spaghetti/ast/MethodNode.java
+++ spaghetti-core/src/main/java/com/prezi/spaghetti/ast/MethodNode.java
@@ -1,6 +1,6 @@
 package com.prezi.spaghetti.ast;
 
-public interface MethodNode extends NamedNode, AnnotatedNode {
+public interface MethodNode extends NamedNode, AnnotatedNode, DocumentedNode {
 	NamedNodeSet<TypeParameterNode> getTypeParameters();
 	TypeReference getReturnType();
 	NamedNodeSet<MethodParameterNode> getParameters();
