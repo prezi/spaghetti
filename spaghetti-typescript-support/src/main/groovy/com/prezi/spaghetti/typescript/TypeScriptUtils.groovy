97bf834134e11f3e2dfb8cff7cbb8c128b17f5d8
Lorant Pinter <lorant.pinter@prezi.com>
2014-10-22 12:52:16 +0200

    Put TypeScript headers in namespace directories
    Fixes #153

--- spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/TypeScriptUtils.groovy
+++ spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/TypeScriptUtils.groovy
@@ -5,7 +5,9 @@ import com.prezi.spaghetti.ast.ModuleNode
 final class TypeScriptUtils {
 	public static File createSourceFile(String header, ModuleNode module, String name, File outputDirectory, String contents) {
 		def namespace = module.name
-		def file = new File(outputDirectory, name + ".ts")
+		def packageDir = createNamespacePath(outputDirectory, namespace)
+		packageDir.mkdirs()
+		def file = new File(packageDir, name + ".ts")
 		file.delete()
 		file << "/*\n"
 		file << " * " + header + "\n"
@@ -20,6 +22,16 @@ final class TypeScriptUtils {
 			file << contents
 		}
 		return file
+	}
+
+	private static File createNamespacePath(File root, String namespace) {
+		File result = root
+		if (namespace) {
+			namespace.split(/\./).each { name ->
+				result = new File(result, name)
+			}
+		}
+		return result
 	}
 
 	public static String toPrimitiveString(Object value) {
