040c0f82c1f8d989da075429c71a363f9a472d9a
Mihály Köles <mihaly.koles@prezi.com>
2014-11-14 12:29:07 +0100

    javascript example class

--- spaghetti-clojure-support/src/resources/external.js
+++ spaghetti-clojure-support/src/resources/external.js
@@ -1,4 +1,43 @@
-function() hello {
-  return "world";
+var JavaScriptExampleObject = {
+  myInt: 42,
+  myString: "fortytwo",
+  myArray: [4,2],
+
+  getInt: function() {
+    return this.myInt;
+  },
+  setInt: function(someInt) {
+    this.myInt = someInt;
+  },
+  getString: function() {
+    return this.myString;
+  },
+  setString: function(someString) {
+    this.myString = someString;
+  },
+  getArray: function() {
+    return this.myArray;
+  },
+  setArray: function(someArray) {
+    this.myArray = someArray;
+  },
+  callBack: function(callBackFunction) {
+    if (typeof callBackFunction!==undefined) {
+      return callBackFunction();
+    }
+  }
 }
 
+console.log(JavaScriptExampleObject.getInt());
+console.log(JavaScriptExampleObject.setInt(43));
+console.log(JavaScriptExampleObject.getInt());
+
+console.log(JavaScriptExampleObject.getString());
+console.log(JavaScriptExampleObject.setString("fortythree"));
+console.log(JavaScriptExampleObject.getString());
+
+console.log(JavaScriptExampleObject.getArray());
+console.log(JavaScriptExampleObject.setArray([4,3]));
+console.log(JavaScriptExampleObject.getArray());
+
+console.log(JavaScriptExampleObject.callBack(function(){return "the meaning of life"}));
