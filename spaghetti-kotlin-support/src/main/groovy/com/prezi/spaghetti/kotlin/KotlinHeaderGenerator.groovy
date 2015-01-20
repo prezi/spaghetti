d99989756ba512c677d86c2f1460cb23ce03503a
Lorant Pinter <lorant.pinter@prezi.com>
2015-01-20 18:47:48 +0100

    Only generate module accessor for direct dependencies

--- spaghetti-kotlin-support/src/main/groovy/com/prezi/spaghetti/kotlin/KotlinHeaderGenerator.groovy
+++ spaghetti-kotlin-support/src/main/groovy/com/prezi/spaghetti/kotlin/KotlinHeaderGenerator.groovy
@@ -25,6 +25,8 @@ class KotlinHeaderGenerator extends AbstractHeaderGenerator {
 		generateModuleTypes(config.localModule, outputDirectory, header)
 		config.allDependentModules.each { dependentModule ->
 			generateModuleTypes(dependentModule, outputDirectory, header)
+		}
+		config.directDependentModules.each { dependentModule ->
 			generateModuleAccessor(dependentModule, outputDirectory, header)
 		}
 	}
