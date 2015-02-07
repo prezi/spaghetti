382b0422a0dab667d4ea3dc9e27e17a406d8f048
Lorant Pinter <lorant.pinter@prezi.com>
2015-02-07 17:10:19 +0100

    Added IT implementation in Haxe

---
+++ spaghetti-haxe-support/src/test/groovy/com/prezi/spaghetti/haxe/HaxeGeneratorIntegrationTest.groovy
@@ -0,0 +1,27 @@
+package com.prezi.spaghetti.haxe
+
+import com.prezi.spaghetti.ast.ModuleNode
+import com.prezi.spaghetti.generator.HeaderGenerator
+import com.prezi.spaghetti.generator.JavaScriptBundleProcessor
+import com.prezi.spaghetti.generator.test.LanguageSupportSpecification
+
+class HaxeGeneratorIntegrationTest extends LanguageSupportSpecification {
+	@Override
+	protected HeaderGenerator createHeaderGenerator() {
+		return new HaxeHeaderGenerator()
+	}
+
+	@Override
+	protected JavaScriptBundleProcessor createBundleProcessor() {
+		return new HaxeJavaScriptBundleProcessor()
+	}
+
+	@Override
+	protected void compile(ModuleNode module, File compiledJs, File headersDir, File sourceDir) {
+		execute "haxe",
+				"-js", compiledJs,
+				"-cp", headersDir,
+				"-cp", sourceDir,
+				"--macro", "include(\'" + module.name + "\')"
+	}
+}
---
+++ spaghetti-haxe-support/src/test/it/src/spaghetti/test/TestModule.hx
@@ -0,0 +1,7 @@
+package spaghetti.test;
+
+class TestModule {
+	public static function add(a:Int, b:Int):Int {
+		return a + b;
+	}
+}
