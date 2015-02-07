9fb81bec7a87d9a6130559bb7ee1469ec4776b7b
Lorant Pinter <lorant.pinter@prezi.com>
2015-02-07 17:08:42 +0100

    Empty module bundle set constant

--- spaghetti-core/src/main/java/com/prezi/spaghetti/bundle/ModuleBundleSet.java
+++ spaghetti-core/src/main/java/com/prezi/spaghetti/bundle/ModuleBundleSet.java
@@ -1,8 +1,13 @@
 package com.prezi.spaghetti.bundle;
 
+import com.prezi.spaghetti.bundle.internal.DefaultModuleBundleSet;
+
+import java.util.Collections;
 import java.util.SortedSet;
 
 public interface ModuleBundleSet extends SortedSet<ModuleBundle> {
+	public static final ModuleBundleSet EMPTY = new DefaultModuleBundleSet(Collections.<ModuleBundle>emptySet(), Collections.<ModuleBundle>emptySet());
+
 	SortedSet<ModuleBundle> getDirectBundles();
 	SortedSet<ModuleBundle> getTransitiveBundles();
 }
