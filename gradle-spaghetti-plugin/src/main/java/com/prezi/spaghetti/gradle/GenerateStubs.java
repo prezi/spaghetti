630e91222290bfb47b72c0cee03b750f0d842c18
Lorant Pinter <lorant.pinter@prezi.com>
2015-04-10 12:02:04 +0200

    Option to switch off timestamps in generated code
    Quick fix for issue #179

--- gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/GenerateStubs.java
+++ gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/GenerateStubs.java
@@ -7,6 +7,7 @@ import com.prezi.spaghetti.generator.internal.DefaultGeneratorParameters;
 import com.prezi.spaghetti.generator.internal.InternalGeneratorUtils;
 import com.prezi.spaghetti.gradle.internal.AbstractDefinitionAwareSpaghettiTask;
 import org.apache.commons.io.FileUtils;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
@@ -16,6 +17,8 @@ import java.util.concurrent.Callable;
 
 public class GenerateStubs extends AbstractDefinitionAwareSpaghettiTask {
 	private File outputDirectory;
+
+	private boolean timestamp = true;
 
 	@TaskAction
 	public void generate() throws IOException {
@@ -52,5 +55,16 @@ public class GenerateStubs extends AbstractDefinitionAwareSpaghettiTask {
 		});
 	}
 
+	@Input
+	public boolean getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(boolean timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public void timestamp(boolean timestamp) {
+		setTimestamp(timestamp);
 	}
 }
