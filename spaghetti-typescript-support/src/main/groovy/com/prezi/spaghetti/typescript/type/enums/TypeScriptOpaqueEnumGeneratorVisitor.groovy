87485588e71c4bcbe408537bb3bb9e4a253b49e1
Laszlo Pandy <laszlo.pandy@prezi.com>
2016-03-03 18:01:25 +0100

    Use declare, so empty enums don’t generate useless code.

--- spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/type/enums/TypeScriptOpaqueEnumGeneratorVisitor.groovy
+++ spaghetti-typescript-support/src/main/groovy/com/prezi/spaghetti/typescript/type/enums/TypeScriptOpaqueEnumGeneratorVisitor.groovy
@@ -6,7 +6,7 @@ import com.prezi.spaghetti.typescript.AbstractTypeScriptGeneratorVisitor
 class TypeScriptOpaqueEnumGeneratorVisitor extends AbstractTypeScriptGeneratorVisitor {
 	@Override
 	String visitEnumNode(EnumNode node) {
-"""export enum ${node.name} {
+"""export declare enum ${node.name} {
 	// Enum members are not generated for transitive dependencies. To generate
 	// the enum type with members, depend directly on the containing module.
 }
