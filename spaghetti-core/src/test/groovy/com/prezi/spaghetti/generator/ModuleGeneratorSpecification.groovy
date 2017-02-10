439abfca5644f1dfd6d151e7dacbb516c03f7aa7
Laszlo Pandy <laszlo.pandy@prezi.com>
2017-02-10 15:15:25 +0100

    Make ModuleParser abstract.

--- spaghetti-core/src/test/groovy/com/prezi/spaghetti/generator/ModuleGeneratorSpecification.groovy
+++ spaghetti-core/src/test/groovy/com/prezi/spaghetti/generator/ModuleGeneratorSpecification.groovy
@@ -3,12 +3,12 @@ package com.prezi.spaghetti.generator
 import com.prezi.spaghetti.ast.ModuleVisitorBase
 import com.prezi.spaghetti.ast.ReferableTypeNode
 import com.prezi.spaghetti.ast.internal.parser.Locator
-import com.prezi.spaghetti.ast.internal.parser.ModuleParser
+import com.prezi.spaghetti.ast.internal.parser.DefaultModuleParser
 
 class ModuleGeneratorSpecification extends AbstractGeneratorSpecification {
 	protected <T> T parseAndVisitModule(String fragment, ModuleVisitorBase<T> visitor, ReferableTypeNode... existingTypes) {
 		return parseAndVisitNode(fragment, visitor, existingTypes) { Locator locator, com.prezi.spaghetti.internal.grammar.ModuleParser moduleParser ->
-			return new ModuleParser(locator, moduleParser.moduleDefinition())
+			return new DefaultModuleParser(locator, moduleParser.moduleDefinition())
 		}
 	}
 }
