0d6830e38e1762100a0625b0402c33bd85dee26a
laszlopandy <laszlo.pandy@prezi.com>
2017-12-18 15:54:17 +0100

    MergeDtsTask uses ts api to merge imported files into .d.ts

--- gradle-spaghetti-typescript-plugin/src/main/java/com/prezi/spaghetti/typescript/gradle/internal/MergeDtsTask.java
+++ gradle-spaghetti-typescript-plugin/src/main/java/com/prezi/spaghetti/typescript/gradle/internal/MergeDtsTask.java
@@ -8,8 +8,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.NameFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.gradle.api.Task;
-import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFile;
@@ -17,28 +15,30 @@ import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.SourceTask;
 import org.gradle.api.tasks.TaskAction;
 import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import com.prezi.spaghetti.definition.DefinitionFile;
+import com.prezi.spaghetti.gradle.NeedsTypeScriptCompilerSpaghettiTask;
 import com.prezi.spaghetti.gradle.internal.DefinitionAwareSpaghettiTask;
-import com.prezi.typescript.gradle.TypeScriptCompileDts;
+import com.prezi.spaghetti.tsast.TypeScriptAstParserException;
+import com.prezi.spaghetti.tsast.TypeScriptAstParserService;
 
-public class MergeDtsTask extends SourceTask implements DefinitionAwareSpaghettiTask {
+public class MergeDtsTask extends SourceTask implements DefinitionAwareSpaghettiTask, NeedsTypeScriptCompilerSpaghettiTask {
 
     private DefinitionFile definition = null;
     private File workDir;
-	private File sourceDir;
+    private File sourceDir;
+    private File tsCompilerPath = null;
 
-	@Input
-	public File getWorkDir() {
-		return workDir;
-	}
+    @Input
+    public File getWorkDir() {
+        return workDir;
+    }
 
-	public void setWorkDir(File dir) {
-		workDir = dir;
-	}
+    public void setWorkDir(File dir) {
+        workDir = dir;
+    }
 
     @InputFile
     public File getDefinitionFile() {
@@ -55,22 +55,31 @@ public class MergeDtsTask extends SourceTask implements DefinitionAwareSpaghetti
     }
 
     @InputDirectory
-	public File getSourceDir() {
-		return sourceDir;
-	}
+    public File getSourceDir() {
+        return sourceDir;
+    }
 
-	public void setSourceDir(File dir) {
-		sourceDir = dir;
-	}
+    public void setSourceDir(File dir) {
+        sourceDir = dir;
+    }
 
-	@OutputFile
-	public File getOutputFile() {
-		return new File(getWorkDir(), getDefinitionDtsFilename());
+    @OutputFile
+    public File getOutputFile() {
+        return new File(getWorkDir(), getDefinitionDtsFilename());
     }
 
     private String getDefinitionDtsFilename() {
         String definitionFilename = FilenameUtils.removeExtension(getDefinition().getFile().getName());
         return definitionFilename + ".d.ts";
+    }
+
+    @InputDirectory
+    public File getCompilerPath() {
+        return tsCompilerPath;
+    }
+
+    public void setCompilerPath(File compilerDir) {
+        this.tsCompilerPath = compilerDir;
     }
 
 
@@ -88,10 +97,25 @@ public class MergeDtsTask extends SourceTask implements DefinitionAwareSpaghetti
         File generatedDts = Iterables.getOnlyElement(files);
         List<String> lines = Lists.newArrayList();
 
-        lines.addAll(Files.asCharSource(generatedDts, Charsets.UTF_8).readLines());
-        lines.add(String.format("export as namespace %s;", getDefinition().getNamespaceOverride()));
-        String content = Joiner.on("\n").join(lines);
+        File mergedDts = new File(getWorkDir(), "_merged.d.ts");
+        try {
+            TypeScriptAstParserService.mergeDefinitionFileImports(
+                getWorkDir(),
+                getCompilerPath(),
+                generatedDts,
+                mergedDts,
+                getLogger()
+            );
+        } catch (TypeScriptAstParserException e) {
+            for (String line: e.getOutput()) {
+                System.out.println(line);
+            }
+            throw new RuntimeException("Merge of TypeScript module definition failed, see console for errors.", e);
+        }
 
+        lines.addAll(Files.asCharSource(mergedDts, Charsets.UTF_8).readLines());
+        lines.add(String.format("export as namespace %s;", getDefinition().getNamespaceOverride()));
+        String content = ReferenceDirectiveStripper.stripAndJoin(lines);
         Files.write(content, getOutputFile(), Charsets.UTF_8);
     }
 }
