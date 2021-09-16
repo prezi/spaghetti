a0ee8b750c5a4a56e2b10c2b286f8df791997569
Sandor Fehervari <sandor.fehervari@prezi.com>
2021-09-16 18:13:42 +0200

    Proper gradle 7 support

--- gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/internal/incubating/DefaultBinaryContainer.java
+++ gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/internal/incubating/DefaultBinaryContainer.java
@@ -1,10 +1,11 @@
 package com.prezi.spaghetti.gradle.internal.incubating;
 
+import org.gradle.api.internal.CollectionCallbackActionDecorator;
 import org.gradle.api.internal.DefaultPolymorphicDomainObjectContainer;
 import org.gradle.internal.reflect.Instantiator;
 
 public class DefaultBinaryContainer extends DefaultPolymorphicDomainObjectContainer<Binary> implements BinaryContainer {
-	public DefaultBinaryContainer(Instantiator instantiator) {
-        super(Binary.class, instantiator);
+	public DefaultBinaryContainer(Instantiator instantiator, CollectionCallbackActionDecorator collectionCallbackActionDecorator) {
+        super(Binary.class, instantiator, collectionCallbackActionDecorator);
     }
 }
--- gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/internal/incubating/DefaultFunctionalSourceSet.java
+++ gradle-spaghetti-plugin/src/main/java/com/prezi/spaghetti/gradle/internal/incubating/DefaultFunctionalSourceSet.java
@@ -1,13 +1,14 @@
 package com.prezi.spaghetti.gradle.internal.incubating;
 
+import org.gradle.api.internal.CollectionCallbackActionDecorator;
 import org.gradle.api.internal.DefaultPolymorphicDomainObjectContainer;
 import org.gradle.internal.reflect.Instantiator;
 
 public class DefaultFunctionalSourceSet extends DefaultPolymorphicDomainObjectContainer<LanguageSourceSet> implements FunctionalSourceSet {
 	private final String name;
 
-    public DefaultFunctionalSourceSet(String name, Instantiator instantiator) {
-        super(LanguageSourceSet.class, instantiator);
+    public DefaultFunctionalSourceSet(String name, Instantiator instantiator, CollectionCallbackActionDecorator collectionCallbackActionDecorator) {
+        super(LanguageSourceSet.class, instantiator, collectionCallbackActionDecorator);
         this.name = name;
     }
 
